601ba16ec16a187cdfbf752568686b8ccbd3a24f
Levente Polyak <levente@leventepolyak.net>
2021-09-12 22:05:59 +0200

    chore(cleanup): use clippy recommended else unwrapping

--- src/config.rs
+++ src/config.rs
@@ -93,112 +93,85 @@ impl Config {
             },
             format: if let Some(format) = &args.format {
                 format.into()
-            } else if let Some(format) = GitConfigBuilder::new("smash.format").get()? {
-                format
             } else {
-                DEFAULT_LIST_FORMAT.into()
+                GitConfigBuilder::new("smash.format")
+                    .get()?
+                    .unwrap_or_else(|| DEFAULT_LIST_FORMAT.into())
             },
             max_count: if let Some(max_count) = args.max_count {
                 max_count
-            } else if let Some(max_count) = GitConfigBuilder::new("smash.maxCommitCount")
-                .with_type("int")
-                .with_default("0")
-                .get_as_int()?
-            {
-                max_count
             } else {
-                0
+                GitConfigBuilder::new("smash.maxCommitCount")
+                    .with_type("int")
+                    .with_default("0")
+                    .get_as_int()?
+                    .unwrap_or(0)
             },
             auto_rebase: if args.rebase {
                 true
             } else if args.no_rebase {
                 false
-            } else if let Some(auto_rebase) = GitConfigBuilder::new("smash.autorebase")
-                .with_type("bool")
-                .with_default("true")
-                .get_as_bool()?
-            {
-                auto_rebase
             } else {
-                true
+                GitConfigBuilder::new("smash.autorebase")
+                    .with_type("bool")
+                    .with_default("true")
+                    .get_as_bool()?
+                    .unwrap_or(true)
             },
             interactive: if args.interactive {
                 true
-            } else if let Some(interactive) = GitConfigBuilder::new("smash.interactive")
-                .with_type("bool")
-                .with_default("false")
-                .get_as_bool()?
-            {
-                interactive
             } else {
-                false
+                GitConfigBuilder::new("smash.interactive")
+                    .with_type("bool")
+                    .with_default("false")
+                    .get_as_bool()?
+                    .unwrap_or(false)
             },
             blame: if args.blame {
                 true
             } else if args.no_blame {
                 false
-            } else if let Some(blame) = GitConfigBuilder::new("smash.blame")
-                .with_type("bool")
-                .with_default("true")
-                .get_as_bool()?
-            {
-                blame
             } else {
-                true
+                GitConfigBuilder::new("smash.blame")
+                    .with_type("bool")
+                    .with_default("true")
+                    .get_as_bool()?
+                    .unwrap_or(true)
             },
             files: if args.files {
                 true
             } else if args.no_files {
                 false
-            } else if let Some(files) = GitConfigBuilder::new("smash.files")
-                .with_type("bool")
-                .with_default("true")
-                .get_as_bool()?
-            {
-                files
             } else {
-                true
+                GitConfigBuilder::new("smash.files")
+                    .with_type("bool")
+                    .with_default("true")
+                    .get_as_bool()?
+                    .unwrap_or(true)
             },
             recent: if args.recent {
                 true
             } else if args.no_recent {
                 false
-            } else if let Some(recent) = GitConfigBuilder::new("smash.recent")
-                .with_type("bool")
-                .with_default("false")
-                .get_as_bool()?
-            {
-                recent
             } else {
-                false
+                GitConfigBuilder::new("smash.recent")
+                    .with_type("bool")
+                    .with_default("false")
+                    .get_as_bool()?
+                    .unwrap_or(false)
             },
-            source_label_files: if let Some(color) =
-                GitConfigBuilder::new("smash.filesSourceFormat")
-                    .with_default(DEFAULT_FORMAT_SOURCE_FILES)
-                    .get()?
-            {
-                color
-            } else {
-                DEFAULT_FORMAT_SOURCE_FILES.into()
-            },
-            source_label_blame: if let Some(color) =
-                GitConfigBuilder::new("smash.blameSourceFormat")
-                    .with_default(DEFAULT_FORMAT_SOURCE_BLAME)
-                    .get()?
-            {
-                color
-            } else {
-                DEFAULT_FORMAT_SOURCE_BLAME.into()
-            },
-            source_label_recent: if let Some(color) =
-                GitConfigBuilder::new("smash.recentSourceFormat")
-                    .with_default(DEFAULT_FORMAT_SOURCE_RECENT)
-                    .get()?
-            {
-                color
-            } else {
-                DEFAULT_FORMAT_SOURCE_RECENT.into()
-            },
+            source_label_files: GitConfigBuilder::new("smash.filesSourceFormat")
+                .with_default(DEFAULT_FORMAT_SOURCE_FILES)
+                .get()?
+                .unwrap_or_else(|| DEFAULT_FORMAT_SOURCE_FILES.into()),
+            source_label_blame: GitConfigBuilder::new("smash.blameSourceFormat")
+                .with_default(DEFAULT_FORMAT_SOURCE_BLAME)
+                .get()?
+                .unwrap_or_else(|| DEFAULT_FORMAT_SOURCE_BLAME.into()),
+            source_label_recent: GitConfigBuilder::new("smash.recentSourceFormat")
+                .with_default(DEFAULT_FORMAT_SOURCE_RECENT)
+                .get()?
+                .unwrap_or_else(|| DEFAULT_FORMAT_SOURCE_RECENT.into()),
             commit: args.commit.clone(),
             fixup_mode: if args.amend {
                 git_check_version(&git_version, ">=2.33", "--amend")?;
--- src/main.rs
+++ src/main.rs
@@ -345,6 +345,8 @@ fn spawn_menu() -> Result<Child> {
         .spawn()?)
 }
 
+// TODO: remove once stabilized
+#[allow(unknown_lints, clippy::manual_split_once)]
 fn select_target(line: &[u8]) -> Result<String> {
     let cow = String::from_utf8_lossy(line);
     Ok(cow
